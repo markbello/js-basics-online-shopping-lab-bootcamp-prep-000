6fa03022df00655b108435e8928a9cfe982be5e2
Mark Bello <mcbello51286@gmail.com>
2017-11-17 19:29:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function total(){
 
 function removeFromCart(item){
   for (var i = 0; i < cart.length; i++) {
-  	if(item in Object.keys(cart[i])){
+  	if(cart[i].hasOwnProperty(item)){
   	  cart.splice(i,1);
   	  return cart;
   	}
